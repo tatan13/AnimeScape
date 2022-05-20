f33e6f4a3c2af31334768e7b4d0e6b8102e6f05c
tatan13 <fa78577@cg7.so-net.ne.jp>
2022-05-20 23:59:55 +0900

    add ogp

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,12 +1,20 @@
 <!DOCTYPE html>
 <html lang="ja">
 
-<head>
+<head prefix="og: https://ogp.me/ns#">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <meta property="og:url" content="https://www.animescape.link/" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="AnimeScape" />
+    <meta property="og:description" content="アニメ批評サービス" />
+    <meta property="og:site_name" content="AnimeScape" />
+    <meta property="og:image" content="https://www.animescape.link/animescape_ogp.png" />
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:site" content="@tatan_tech" />
     @yield('title')
     @if (env('APP_ENV') == 'production')
         <!-- Global site tag (gtag.js) - Google Analytics -->
