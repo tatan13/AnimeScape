34a5609f8788adac421f59540549cce194a5a009
tatan13 <fa78577@cg7.so-net.ne.jp>
2022-11-22 04:21:23 +0900

    add: favicon

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -16,6 +16,10 @@
     <meta name="twitter:card" content="summary" />
     <meta name="twitter:site" content="@tatan_tech" />
     <meta name="keywords" content="アニメ,アニメスケープ,アニスケ">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
     @yield('title')
     @if (env('APP_ENV') == 'production')
         <!-- Global site tag (gtag.js) - Google Analytics -->
