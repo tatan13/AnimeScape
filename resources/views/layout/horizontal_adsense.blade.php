aa8cb542cc25648479170e30347be30516e6a5cd
tatan13 <fa78577@cg7.so-net.ne.jp>
2022-11-23 03:06:39 +0900

    improve: delete adsense

--- resources/views/layout/horizontal_adsense.blade.php
+++ resources/views/layout/horizontal_adsense.blade.php
@@ -1,10 +1,10 @@
-<div class="title_adsense col-md-8">
+{{-- <div class="title_adsense col-md-8">
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1583558174306860"
         crossorigin="anonymous"></script>
     <!-- 横長ディスプレイ -->
-    <ins class="adsbygoogle" style="display:inline-block;width:728px;height:90px" data-ad-client="ca-pub-1583558174306860"
-        data-ad-slot="3151137941"></ins>
+    <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-1583558174306860" data-ad-slot="3151137941"
+        data-ad-format="auto" data-full-width-responsive="true"></ins>
     <script>
         (adsbygoogle = window.adsbygoogle || []).push({});
     </script>
-</div>
+</div> --}}
