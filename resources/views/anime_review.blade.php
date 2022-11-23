22fd9d1a3a407ee999c1e981acb6ba98b6c86333
tatan13 <fa78577@cg7.so-net.ne.jp>
2022-11-24 06:42:00 +0900

    fix: delete adsense from review page

--- resources/views/anime_review.blade.php
+++ resources/views/anime_review.blade.php
@@ -4,19 +4,6 @@
     <title>{{ $anime->title }}のデータ入力 AnimeScape -アニメ批評空間-</title>
 @endsection
 
-@section('adsense')
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1583558174306860"
-        crossorigin="anonymous"></script>
-@endsection
-
-@section('title_adsense')
-    @include('layout.horizontal_adsense')
-@endsection
-
-@section('sidebar_adsense')
-    @include('layout.vertical_adsense')
-@endsection
-
 @section('main')
     <article class="anime_review">
         <h2>{{ $anime->title }}のデータ入力</h2>
