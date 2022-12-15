772ff237063a9e5132cf17a9d5320e82e05a8aa0
tatan13 <fa78577@cg7.so-net.ne.jp>
2022-12-15 13:44:04 +0900

    fix: escape br for comment

--- resources/views/anime_comment.blade.php
+++ resources/views/anime_comment.blade.php
@@ -28,11 +28,11 @@
                     href="{{ route('anime.show', ['anime_id' => $user_review->anime->id]) }}">{{ $user_review->anime->title }}</a>
             </h2>
             <p class="text-info">{{ $user_review->one_word_comment }}</p>
-            <p>{{ $user_review->long_word_comment }}</p>
+            <p>{!! nl2br(e($user_review->long_word_comment)) !!}</p>
             <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button"
-            data-text="{{ $user_review->user->name }}さんの{{ $user_review->anime->title }}の感想{{ !is_null($user_review->score) ? '(' . $user_review->score . '点)' : '' }}{{ ($user_review->spoiler == true) ? '(ネタバレあり)' : '' }}"
-            data-url="{{ route('user_anime_comment.show', ['user_review_id' => $user_review->id]) }}" data-hashtags="AnimeScape"
-            data-related="tatan_tech" data-show-count="false">Tweet</a>
+                data-text="{{ $user_review->user->name }}さんの{{ $user_review->anime->title }}の感想{{ !is_null($user_review->score) ? '(' . $user_review->score . '点)' : '' }}{{ $user_review->spoiler == true ? '(ネタバレあり)' : '' }}"
+                data-url="{{ route('user_anime_comment.show', ['user_review_id' => $user_review->id]) }}"
+                data-hashtags="AnimeScape" data-related="tatan_tech" data-show-count="false">Tweet</a>
             <p>{{ $user_review->comment_timestamp }}</p>
             <hr>
         </section>
